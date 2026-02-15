a7c736c4250008a03a5b5330872d0a45c179e293
Roberto-Sanna <sannaroberto577@gmail.com>
2026-02-15 16:19:18 +0100

    Add .gitattributes and .gitignore.
    Add project files.

---
+++ RecipesApplication/Models/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace RecipesApplication.Models
+{
+    public class Ingredient
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Taste { get; set; }
+    }
+}
---
+++ RecipesApplication/Models/Recipe.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace RecipesApplication.Models
+{
+    public class Recipe
+    {
+        public ObjectId  Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+    }
+}
